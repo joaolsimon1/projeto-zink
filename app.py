e53aee7e056ba4f7d1aed228e6ed53beb0db83ce
Joao Lucas Simon <joao_lucas_simon@hotmail.com>
2025-02-04 19:55:11 -0300

    8

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import pandas as pd
 import plotly.express as px
 from function import process_excel
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide",
+                   page_title="App Zink",  # Título da página
+                 page_icon="⚛️")  # Ícone da página (opcional))
 
 # Configuração da interface do Streamlit
 st.title("Processamento de Dados")
@@ -11,7 +13,7 @@ st.title("Processamento de Dados")
 # Sidebar para upload de arquivo
 with st.sidebar:
     st.header("Envio de Arquivo")
-    uploaded_file = st.file_uploader("Envie o arquivo Excel ou CSV", type=['xlsx', 'csv'])
+    uploaded_file = st.file_uploader("Envie o arquivo Excel ou CSV", type=['xlsx', 'csv', 'txt'])
     st.cache_data.clear()
 
 if uploaded_file:
@@ -21,6 +23,11 @@ if uploaded_file:
             df = pd.read_excel(uploaded_file, engine='openpyxl')
         elif uploaded_file.name.endswith('.csv'):
             df = pd.read_csv(uploaded_file, sep=";", header=None)
+
+        elif uploaded_file.name.endswith('.txt'):
+            # Ler o arquivo TXT
+            df = pd.read_csv(uploaded_file, sep="\t", header=None)  # Usar \t para separador de tabulação
+            
     except Exception as e:
         st.error(f"Erro ao carregar o arquivo: {e}")
 
@@ -64,21 +71,35 @@ if 'df_abs' in st.session_state and 'df_des' in st.session_state:
                 aux1 = aux1['AcumuladoABS'].resample('T').mean().reset_index()
                 aux1.columns = ['DATAHORA', 'AcumuladoABS']
                 aux1 = aux1.loc[pd.notna(aux1['AcumuladoABS'])].reset_index(drop=True)
-                aux1['Min'] = aux1.index
+                aux1['Tempo'] = aux1.index
                 aux1['NUMERO_CICLO'] = i
                 resultados.append(aux1)
             df_todos_ciclos = pd.concat(resultados).reset_index(drop=True)
+
+            df_pivot = df_todos_ciclos.pivot(index='Tempo', columns='NUMERO_CICLO', values='AcumuladoABS')
+
+            df_pivot.columns = [f"Acum ABS C{n}" for n in df_pivot.columns]
+
+            df_pivot = df_pivot.reset_index().rename(columns={'Tempo': 'Tempo'})
+
+            df_pivot = df_pivot.round(3)
+
+            df_pivot = df_pivot.applymap(lambda x: str(x).replace('.', ',') if isinstance(x, (int, float)) else x)
+
             fig = px.line(
                 df_todos_ciclos,
-                x='Min',
+                x='Tempo',
                 y='AcumuladoABS',
                 color='NUMERO_CICLO',
                 title='Absorção'
             )
-            fig.update_layout(xaxis_title="Tempo (Min)", yaxis_title="CO₂ absorvido acumulado (mg)", title_x=0.5)
+            fig.update_layout(xaxis_title="Tempo (Minutos)", yaxis_title="CO₂ absorvido acumulado (mg)", title_x=0.5)
             st.plotly_chart(fig, use_container_width=True)
             st.write("Dataframe utilizado no gráfico de Absorção:")
-            st.dataframe(df_todos_ciclos, use_container_width=True, hide_index=True)
+            #st.dataframe(df_todos_ciclos, use_container_width=True, hide_index=True)
+            st.dataframe(df_pivot, use_container_width=True, hide_index=True)
+
+
             st.download_button(
                 label="Baixar dados de Absorção como .txt",
                 data=df_todos_ciclos.to_csv(index=False, sep="\t"),
@@ -97,21 +118,34 @@ if 'df_abs' in st.session_state and 'df_des' in st.session_state:
                 aux2 = aux2['AcumuladoDES'].resample('T').mean().reset_index()
                 aux2.columns = ['DATAHORA', 'AcumuladoDES']
                 aux2 = aux2.loc[pd.notna(aux2['AcumuladoDES'])].reset_index(drop=True)
-                aux2['Min'] = aux2.index
+                aux2['Tempo'] = aux2.index
                 aux2['NUMERO_CICLO'] = i
                 resultados.append(aux2)
             df_todos_ciclos2 = pd.concat(resultados).reset_index(drop=True)
+
+            df_pivot2 = df_todos_ciclos2.pivot(index='Tempo', columns='NUMERO_CICLO', values='AcumuladoDES')
+
+            df_pivot2.columns = [f"Acum DES C{n}" for n in df_pivot2.columns]
+
+            df_pivot2 = df_pivot2.reset_index().rename(columns={'Tempo': 'Tempo'})
+
+            df_pivot2 = df_pivot2.round(3)
+
+            df_pivot2 = df_pivot2.applymap(lambda x: str(x).replace('.', ',') if isinstance(x, (int, float)) else x)
+
             fig2 = px.line(
                 df_todos_ciclos2,
-                x='Min',
+                x='Tempo',
                 y='AcumuladoDES',
                 color='NUMERO_CICLO',
                 title='Dessorção'
             )
-            fig2.update_layout(xaxis_title="Tempo (Min)", yaxis_title="CO₂ absorvido acumulado (mg)", title_x=0.5)
+            fig2.update_layout(xaxis_title="Tempo (Minutos)", yaxis_title="CO₂ absorvido acumulado (mg)", title_x=0.5)
             st.plotly_chart(fig2, use_container_width=True)
             st.write("Dataframe utilizado no gráfico de Dessorção:")
-            st.dataframe(df_todos_ciclos2, use_container_width=True, hide_index=True)
+            #st.dataframe(df_todos_ciclos2, use_container_width=True, hide_index=True)
+            st.dataframe(df_pivot, use_container_width=True, hide_index=True)
+
             st.download_button(
                 label="Baixar dados de Dessorção como .txt",
                 data=df_todos_ciclos2.to_csv(index=False, sep="\t"),
